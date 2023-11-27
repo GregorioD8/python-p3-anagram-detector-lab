b2a1503ecd985a99a64362a30ec119894c56274e
Gregorio <Greg.del801@gmail.com>
2023-11-27 10:41:14 -0700

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        # sort the words letters to compare later
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list): 
+        match_word_list = []
+       
+        # compare letters in words 
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+        return match_word_list
+    
